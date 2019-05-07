a5e0c5c3455f222e4c5786dd99dc3c870c38e020
mm2288 <47225267+mm2288@users.noreply.github.com>
2019-05-07 15:43:55 -0400

    Displays date correctly

--- site.js
+++ site.js
@@ -69,9 +69,61 @@ const vue_app = new Vue({
       methods: {
             /* ADD FUNCTIONS/METHODS FOR STEP 7 HERE */
 
-            /*getMonthText(dateArray) {
+            getMonthText: function (dateArray) {
+              let month = dateArray[1];
+              let day = dateArray[2];
+              let year = dateArray[0];
 
-            },*/
+              switch (month) {
+                case 1:
+                month = 'January';
+                break;
+
+                case 2:
+                month = 'February';
+                break;
+
+                case 3:
+                month = "March";
+                break;
+
+                case 4:
+                month = "April";
+                break;
+
+                case 5:
+                month = "May";
+                break;
+
+                case 6:
+                month = "June";
+                break;
+
+                case 7:
+                month = "July";
+                break;
+
+                case 8:
+                month = "August";
+                break;
+
+                case 9:
+                month = "September";
+                break;
+
+                case 10:
+                month = "October";
+                break;
+
+                case 11:
+                month = "November";
+                break;
+
+                case 12:
+                month = "December";
+              }
+              return (month + " " + day + ", " + year);
+            },
 
             like: function (index) {
               this.movies[index].likes++;
